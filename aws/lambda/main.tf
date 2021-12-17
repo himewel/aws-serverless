dd575a545c121ba84b1532ddaac5521f7a2aa9b9
Welbert Castro <himewel@users.noreply.github.com>
2021-12-16 23:03:58 -0300

    Receive only csv files

--- aws/lambda/main.tf
+++ aws/lambda/main.tf
@@ -59,6 +59,7 @@ resource "aws_iam_role_policy" "logs_policy" {
 
 resource "aws_lambda_function" "landing_to_sqs_lambda" {
   function_name = "landing_to_sqs-2152"
+  depends_on    = [aws_iam_role_policy.logs_policy]
   image_uri     = "343221145296.dkr.ecr.sa-east-1.amazonaws.com/functions-2152:latest"
   package_type  = "Image"
   role          = aws_iam_role.role_lambda.arn
@@ -67,23 +68,22 @@ resource "aws_lambda_function" "landing_to_sqs_lambda" {
   image_config {
     command = ["landing_to_sqs.lambda_handler"]
   }
-
-  depends_on = [aws_iam_role_policy.logs_policy]
 }
 
 resource "aws_lambda_permission" "allow_bucket_lambda_alias" {
-  action  = "lambda:InvokeFunction"
-  function_name  = "${aws_lambda_function.landing_to_sqs_lambda.arn}"
-  principal = "s3.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.landing_to_sqs_lambda.arn}"
+  principal     = "s3.amazonaws.com"
   source_arn    = "${aws_s3_bucket.landing_bucket.arn}"
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.landing_bucket.id
+  bucket     = aws_s3_bucket.landing_bucket.id
   depends_on = [aws_lambda_permission.allow_bucket_lambda_alias]
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.landing_to_sqs_lambda.arn
     events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".csv"
   }
 }
